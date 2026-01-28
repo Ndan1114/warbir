e29b29307ac57e095b5f3060e90077909f78ccc4
Muhammad Susilo R <164564318+Ndan1114@users.noreply.github.com>
2026-01-27 22:32:29 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -499,4 +499,4 @@ def check_new_orders():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
